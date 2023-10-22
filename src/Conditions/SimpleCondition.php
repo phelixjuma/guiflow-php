ad70f57ff702e550573897a419ab8aefc33c16e4
Phelix <phelix@Phelixs-MacBook-Pro.local>
2023-10-22 19:55:15 +0300

    not exists checks

--- src/Conditions/SimpleCondition.php
+++ src/Conditions/SimpleCondition.php
@@ -102,16 +102,12 @@ class SimpleCondition implements ConditionInterface
                 // For arrays, we remove empty values
                 if (is_array($pathValue) && !Utils::isObject($pathValue)) {
                     $pathValue = array_filter($pathValue);
-                } else {
-                    $pathValue = Utils::removeExtraSpaces(trim($pathValue));
                 }
                 return $pathValue == 0 || !empty($pathValue);
             case 'not exists':
                 // For arrays, we remove empty values
                 if (is_array($pathValue) && !Utils::isObject($pathValue)) {
                     $pathValue = array_filter($pathValue);
-                } else {
-                    $pathValue = Utils::removeExtraSpaces(trim($pathValue));
                 }
                 return !($pathValue == 0 || !empty($pathValue));
             case 'regex':
