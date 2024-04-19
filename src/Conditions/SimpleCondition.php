34c38bcf7b5078c68aa66ec594eebb21db0b0cc8
Phelix <phelix@Phelixs-MacBook-Pro.local>
2024-04-19 13:02:19 +0300

    condition exception

--- src/Conditions/SimpleCondition.php
+++ src/Conditions/SimpleCondition.php
@@ -64,8 +64,10 @@ class SimpleCondition implements ConditionInterface
      * @param $pathValue
      * @param $operator
      * @param $value
+     * @param $similarityThreshold
+     * @param $tokenizeSimilarity
      * @return bool
-     * @throws UnknownOperatorException
+     * @throws \Exception
      */
     public static function compare($pathValue, $operator, $value, $similarityThreshold = null, $tokenizeSimilarity = false): bool
     {
@@ -76,111 +78,117 @@ class SimpleCondition implements ConditionInterface
         $value = is_string($value) ? Utils::cleanText($value)  : $value;
 
         // The existing switch case logic...
-        switch ($operator) {
-            case '==':
-                return $pathValue == $value;
-            case '!=':
-                return $pathValue != $value;
-            case 'gt':
-                return $pathValue > $value;
-            case 'gte':
-                return $pathValue >= $value;
-            case 'lt':
-                return $pathValue < $value;
-            case 'lte':
-                return $pathValue <= $value;
-            case 'contains':
-                return !empty($pathValue) && !empty($value) && str_contains($pathValue, $value);
-            case 'not contains':
-                return empty($pathValue) || empty($value) || !str_contains($pathValue, $value);
-            case 'matches':
-                return preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
-            case 'not matches':
-                return !preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
-            case 'exists':
-                // For arrays, we remove empty values
-                if (is_array($pathValue) && !Utils::isObject($pathValue)) {
-                    $pathValue = array_filter($pathValue);
-                }
-                return $pathValue === 0 || !empty($pathValue);
-            case 'not exists':
-                // For arrays, we remove empty values
-                if (is_array($pathValue) && !Utils::isObject($pathValue)) {
-                    $pathValue = array_filter($pathValue);
-                }
-                return empty($pathValue) && $pathValue !== 0;
-            case 'regex':
-                return preg_match($value, $pathValue) === 1;
-            case 'in':
-                return in_array($pathValue, (array)$value);
-            case 'not in':
-                return !in_array($pathValue, (array)$value);
-            case 'in list all':
-                if (is_array($value)) {
-                    foreach ($value as $v) {
-                        $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
-                        if (!preg_match($pattern, $pathValue)) {
-                            return false;
-                        }
+        try {
+            switch ($operator) {
+                case '==':
+                    return $pathValue == $value;
+                case '!=':
+                    return $pathValue != $value;
+                case 'gt':
+                    return $pathValue > $value;
+                case 'gte':
+                    return $pathValue >= $value;
+                case 'lt':
+                    return $pathValue < $value;
+                case 'lte':
+                    return $pathValue <= $value;
+                case 'contains':
+                    return !empty($pathValue) && !empty($value) && str_contains($pathValue, $value);
+                case 'not contains':
+                    return empty($pathValue) || empty($value) || !str_contains($pathValue, $value);
+                case 'matches':
+                    return preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
+                case 'not matches':
+                    return !preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
+                case 'exists':
+                    // For arrays, we remove empty values
+                    if (is_array($pathValue) && !Utils::isObject($pathValue)) {
+                        $pathValue = array_filter($pathValue);
                     }
-                    return true;
-                }
-                return preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
-            case 'not in list all':
-                if (is_array($value)) {
-                    foreach ($value as $v) {
-                        $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
-                        if (preg_match($pattern, $pathValue)) {
-                            return false;
-                        }
+                    return $pathValue === 0 || !empty($pathValue);
+                case 'not exists':
+                    // For arrays, we remove empty values
+                    if (is_array($pathValue) && !Utils::isObject($pathValue)) {
+                        $pathValue = array_filter($pathValue);
                     }
-                    return true;
-                }
-                return !preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
-            case 'in list any':
-                if (is_array($value)) {
-                    foreach ($value as $v) {
-                        $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
+                    return empty($pathValue) && $pathValue !== 0;
+                case 'regex':
+                    return preg_match($value, $pathValue) === 1;
+                case 'in':
+                    return in_array($pathValue, (array)$value);
+                case 'not in':
+                    return !in_array($pathValue, (array)$value);
+                case 'in list all':
+                    if (is_array($value)) {
+                        foreach ($value as $v) {
+                            $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
+                            if (!preg_match($pattern, $pathValue)) {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    return preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
+                case 'not in list all':
+                    if (is_array($value)) {
+                        foreach ($value as $v) {
+                            $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
+                            if (preg_match($pattern, $pathValue)) {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    return !preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
+                case 'in list any':
+                    if (is_array($value)) {
+                        foreach ($value as $v) {
+                            $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
 
-                        if (preg_match($pattern, $pathValue)) {
-                            //print "\n$pathValue matched to $pattern preped from $v\n";
-                            return true;
-                        } else {
-                            //print "\n$pathValue failed to match $pattern preped from $v\n";
+                            if (preg_match($pattern, $pathValue)) {
+                                //print "\n$pathValue matched to $pattern preped from $v\n";
+                                return true;
+                            }
+                            if (preg_last_error() !== PREG_NO_ERROR) {
+                                //print "Preg Error when matching $pathValue against $pattern : ".Utils::getPregError(preg_last_error());
+                                throw new \Exception("Preg Error when matching ".json_encode($pathValue)." against ".json_encode($pattern)." : ".Utils::getPregError(preg_last_error()));
+                            }
                         }
-                        if (preg_last_error() !== PREG_NO_ERROR) {
-                            //print "Preg Error when matching $pathValue against $pattern : ".Utils::getPregError(preg_last_error());
-                            //throw new \Exception("Preg Error: ".Utils::getPregError(preg_last_error()));
-                        }
+                        return false;
                     }
-                    return false;
-                }
-                return preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
-            case 'not in list any':
-                if (is_array($value)) {
-                    foreach ($value as $v) {
-                        $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
-                        if (!preg_match($pattern, $pathValue)) {
-                            return true;
+                    return preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
+                case 'not in list any':
+                    if (is_array($value)) {
+                        foreach ($value as $v) {
+                            $pattern = '/' . Utils::custom_preg_escape(Utils::full_unescape($v)) . '/i';
+                            if (!preg_match($pattern, $pathValue)) {
+                                return true;
+                            }
+                            if (preg_last_error() !== PREG_NO_ERROR) {
+                                //print "Preg Error when matching $pathValue against $pattern : ".Utils::getPregError(preg_last_error());
+                                throw new \Exception("Preg Error when matching ".json_encode($pathValue)." against ".json_encode($pattern)." : ".Utils::getPregError(preg_last_error()));
+                            }
                         }
+                        return false;
                     }
-                    return false;
-                }
-                return !preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
-            case 'true':
-                return $pathValue === true;
-            case 'false':
-                return $pathValue === false;
-            case 'like':
-                $pattern = str_replace('%', '.*', $value);
-                return preg_match("/$pattern/", $pathValue) === 1;
-            case 'similar_to':
-                if ($tokenizeSimilarity) {
-                    return $fuzz->tokenSortPartialRatio($pathValue, $value) >= $similarityThreshold;
-                }
-                return $fuzz->partialRatio($pathValue, $value) >= $similarityThreshold;
-            default:
-                throw new UnknownOperatorException("Unknown operator: $operator");
+                    return !preg_match('/' . Utils::custom_preg_escape(Utils::full_unescape($value)) . '/i', $pathValue);
+                case 'true':
+                    return $pathValue === true;
+                case 'false':
+                    return $pathValue === false;
+                case 'like':
+                    $pattern = str_replace('%', '.*', $value);
+                    return preg_match("/$pattern/", $pathValue) === 1;
+                case 'similar_to':
+                    if ($tokenizeSimilarity) {
+                        return $fuzz->tokenSortPartialRatio($pathValue, $value) >= $similarityThreshold;
+                    }
+                    return $fuzz->partialRatio($pathValue, $value) >= $similarityThreshold;
+                default:
+                    throw new UnknownOperatorException("Unknown operator: $operator");
+            }
+        } catch (\Exception | \Throwable $e) {
+            throw new \Exception("Comparison error on value: ".json_encode($value)." and path value: ".json_encode($pathValue).". Error says ".$e->getMessage(). ". Line: ".$e->getLine()." on file: ".$e->getFile());
         }
     }
 }
