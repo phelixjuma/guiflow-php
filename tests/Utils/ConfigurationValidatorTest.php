a1e2fef3ce6b584df9258155de905ad7e482af0f
Phelix <phelix@Phelixs-MacBook-Pro.local>
2024-05-07 09:03:39 +0300

    schema v3 update

--- tests/Utils/ConfigurationValidatorTest.php
+++ tests/Utils/ConfigurationValidatorTest.php
@@ -7,14 +7,14 @@ use PHPUnit\Framework\TestCase;
 
 class ConfigurationValidatorTest extends TestCase
 {
-    public function _testValidate()
+    public function testValidate()
     {
 
         $config_json = file_get_contents(dirname(__DIR__) ."/config.json");
         $config = json_decode($config_json);
 
         // validate the config
-        $valid = ConfigurationValidator::validate($config, 'v2');
+        $valid = ConfigurationValidator::validate($config, 'v3');
 
         if ($valid) {
             print "\nValid\n";
