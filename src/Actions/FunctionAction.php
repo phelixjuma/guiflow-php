60176aaa4d4065dffeaf216ee43885843573c694
Phelix <phelix@Phelixs-MacBook-Pro.local>
2024-09-19 09:29:49 +0300

    resolve params for udf

--- src/Actions/FunctionAction.php
+++ src/Actions/FunctionAction.php
@@ -278,9 +278,10 @@ class FunctionAction implements ActionInterface
             $functionName = $paramValues[1];
 
             // We get the UDF's params which should exclude index 1 - function name).
-            //$functionParams = array_slice($paramValues, 2);
+            $functionParams = array_merge([$paramValues[0], array_slice($paramValues, 2)]);
+
             // We get the function params
-            $functionParams = self::resolveParam($paramValues[0], array_slice($paramValues, 2)) ?? [];
+            $functionParams = self::resolveParam($paramValues[0], $functionParams) ?? [];
 
             // We call the user defined function
             $newValue = call_user_func_array([$this->function[0],$functionName], $functionParams);
