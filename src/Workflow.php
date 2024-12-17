935bfed33eca23f0b3a000d5d6b99894c4ca92be
Phelix <phelix@Phelixs-MacBook-Pro.local>
2024-12-17 20:16:17 +0200

    debug

--- src/Workflow.php
+++ src/Workflow.php
@@ -104,7 +104,7 @@ class Workflow
                         // We fetch the results from all the tasks
                         //$results = batch($tasks);
 
-                        $results = Parallel::parallelBatch($tasks, null, "Rules_".$rule['stage']);
+                        $results = Parallel::parallelBatch($tasks, "Rules_".$rule['stage']);
 
                         // Flatten the results and merge them into $tempData
                         foreach ($results as $result) {
@@ -233,7 +233,7 @@ class Workflow
                             }
 
                             // We fetch the results from all the tasks
-                            $results = Parallel::parallelBatch($tasks, null, "Actions_".$action['stage']);
+                            $results = Parallel::parallelBatch($tasks, "Actions_".$action['stage']);
 
                             // Flatten the results and merge them into $tempData
                             foreach ($results as $result) {
